0af0da8761951c32fbce09e769a3cafd5eae1a7c
Nazario Cano <1026187a@umich.mx>
2023-03-25 11:59:56 -0600

    creacion de clase Coche y funcion AnadirPuerta

--- src/main/java/com/devnaz/springboot/SpringbootApplication.java
+++ src/main/java/com/devnaz/springboot/SpringbootApplication.java
@@ -3,6 +3,17 @@ package com.devnaz.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Primera parte:
+// Crear una función con tres parámetros que sean números que se suman entre sí.
+// Llamar a la función en el main y darle valores.
+
+// Segunda parte:
+// Crear una clase coche.
+// Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+// Una función que incremente el número de puertas que tiene el coche.
+// Crear un objeto miCoche en el main y añadirle una puerta.
+// Mostrar el número de puertas que tiene el objeto.
+
 @SpringBootApplication
 public class SpringbootApplication {
 
@@ -10,20 +21,25 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 		int param1 = 10;
 		int param2 = 20;
-		int sum = suma(param1, param2);
+		int param3 = 20;
+		int sum = suma(param1, param2, param3);
 		System.out.println(sum);
+
+		Coche miCoche = new Coche();
+		miCoche.AnadirPuerta();
+		System.out.println(miCoche.puertas);
 	}
 
-	public static int suma(int a, int b) {
-		return a + b;
+	public static int suma(int a, int b, int c) {
+		return a + b + c;
 	}
 
 }
 
-class Potato {
-	public int brazos = 0;
+class Coche {
+	public int puertas = 0;
 
-	public void QuitarBrazo() {
-		this.brazos--;
+	public void AnadirPuerta() {
+		this.puertas++;
 	}
 }
